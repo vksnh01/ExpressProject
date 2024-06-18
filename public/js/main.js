bb96b523a088edfccef362340be0cca248a937a7
vksnh01 <vikrantsharna11@gmail.com>
2024-06-18 17:41:10 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+const data = document.getElementById("inputField");
+data.value = "";
+const checkWeather = () => {
+  const time = document.getElementById("time");
+
+  data.value == ""
+    ? alert("please enter city name")
+    : (fetchData(data.value), (time.innerText = dateFunction()));
+};
+
+const fetchData = async (city) => {
+  const curr = document.getElementById("currTemp");
+  const loc = document.getElementById("city");
+  const maxi = document.getElementById("maxTemp");
+  const mini = document.getElementById("minTemp");
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ce472dac06292782f6d67354e2e1eb12&units=metric`
+  );
+  // sys.country
+  const data = await res.json();
+  console.log(data);
+  if (data.cod == "404") {
+    alert("please check the spelling of city name");
+  } else {
+    curr.innerText = data.main.temp;
+    maxi.innerText = `Max-${data.main.temp_max}`;
+    mini.innerText = `Min-${data.main.temp_min}`;
+    loc.innerText = `${data.name},${data.sys.country}`;
+  }
+};
+
+const dateFunction = () => {
+  const months = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "june",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+  ];
+  const days = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"];
+  const date = new Date();
+  const dated = date.getDate();
+  const month = months[date.getMonth()];
+  const day = days[date.getDay()];
+
+  let hour = date.getHours() - 12;
+  let min = date.getMinutes();
+  let period = "am";
+  hour >= 12 ? (period = "pm") : (period = "am");
+  min < 10 ? (min = "0" + min) : "";
+  const currTime = `${day} ${dated} ${month} | ${hour}:${min} ${period}`;
+  return currTime;
+};
---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+
+const PORT = 3000;
+
+const publicPath = path.join(__dirname, "../public");
+const partialPath = path.join(__dirname, "../templete/partials");
+const viewsPath = path.join(__dirname, "../templete/Views");
+
+app.use(express.static(publicPath));
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server start on port no ${PORT}`);
+});
